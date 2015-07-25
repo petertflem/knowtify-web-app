982abf12742fc3144f3156c561d4daed62657a4b
Peter Tollnes Flem <peter.t.flem@gmail.com>
2015-07-25 14:50:53 +0200

    Refactored and worked on the logic and styling for viewing the log.

--- app/wwwroot/assets/javascript/log-handler.js
+++ app/wwwroot/assets/javascript/log-handler.js
@@ -1,74 +1,46 @@
 (function () {
   var socket = io.connect(window.document.location.origin + '/logs');
-  var enabledOptions = {
-    'timestamp': true,
-    'npm-module': true,
-    'javascript-file': false,
-    'function': false,
-    'line-number': false,
-    'log-level-debug': true,
-    'log-level-info': true,
-    'log-level-warning': true,
-    'log-level-error': true,
-    'log-level-fatal': true
-  };
 
   socket.on('logs', function (data) {
     var div = document.createElement('div');
     div.className = 'logEntry';
 
     var message = document.createElement('span');
-    message.innerHTML = ': ' + data.message;
+    message.innerHTML = '$ ' + data.message;
 
-    div.appendChild(createMetaDataElement('timestamp', 'timestamp', data.timestamp));
-    div.appendChild(createMetaDataElement('log-level-' + data.level.toLowerCase(), data.level.toLowerCase(), data.level));
-    div.appendChild(createMetaDataElement('npm-module', 'location-information npm-module', data.origin.npm_module));
-    div.appendChild(createMetaDataElement('javascript-file', 'location-information javascript-file', data.origin.filename));
-    div.appendChild(createMetaDataElement('function', 'location-information function', data.origin.function));
-    div.appendChild(createMetaDataElement('line-number', 'location-information line-number', data.origin.lineNumnber));
+    div.appendChild(createMetaDataElement('timestamp', '', data.date + ' ' + data.time));
+    var logLevel = createMetaDataElement('log-level', data.logLevel, data.logLevel.toUpperCase());
+    logLevel.setAttribute('data-filter', data.logLevel);
+    div.appendChild(logLevel);
+    div.appendChild(createMetaDataElement('npm-module', 'location-information', data.origin.npmModule));
+    div.appendChild(createMetaDataElement('javascript-file', 'location-information', data.origin.filename));
+    div.appendChild(createMetaDataElement('function', 'location-information', data.origin.fn));
+    div.appendChild(createMetaDataElement('line-number', 'location-information', data.origin.lineNumnber));
     div.appendChild(message);
 
     document.getElementById('logs').appendChild(div);
   });
 
-  $('.option').on('click', function () {
-    toggleOption.call(this, $(this).data('option'));
+  $('.metadata div').on('click', function () {
+    $('[data-meta="' + $(this).data('toggle-meta') + '"]').toggle();
+    $(this).toggleClass('check');
+  });
+
+  $('.logging-level div').on('click', function() {
+    $('.logs [data-filter="' + $(this).data('filter') + '"]').parent().toggle();
+    $(this).toggleClass('check');
   });
 
   function createMetaDataElement(metaDataName, cssClass, content) {
     var element = document.createElement('span');
     element.innerHTML = '[' + content + ']';
-    element.className = cssClass + ' ' + metaDataName;
-    element.style.display = enabledOptions[metaDataName]
+    element.className = cssClass;
+    element.setAttribute('data-meta', metaDataName);
+    element.style.display = $('[data-toggle-meta="' + metaDataName + '"]').hasClass('check')
       ? 'inline'
       : 'none';
 
     return element;
   }
-
-  function toggleOption(optionName) {
-    enabledOptions[optionName] = !enabledOptions[optionName];
-    $(this).toggleClass('check');
-
-    enabledOptions[optionName]
-      ? $('.' + optionName).show()
-      : $('.' + optionName).hide();
-  }
-
-  function toggleOptionCheckmarks() {
-    loopObjectProperties(enabledOptions, function (optionEnabled, optionName) {
-      optionEnabled
-        ? $('[data-option="' + optionName + '"]').addClass('check')
-        : $('[data-option="' + optionName + '"]').removeClass('check');
-    });
-  }
-
-  function loopObjectProperties(obj, callback) {
-    for (var key in obj) {
-      callback(obj[key], key);
-    }
-  }
-
-  toggleOptionCheckmarks();
 
 })();
